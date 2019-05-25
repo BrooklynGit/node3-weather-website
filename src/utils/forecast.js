c790fa2d9298a7741c7a3d6172b7f40d0b4bfc58
Travis Travis <visionreleasefilms@gmail.com>
2019-05-25 15:29:20 -0400

    add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-const forcast = (latitude, longitude, callback) => {
+const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/13375759673550f9359a75506979a3f3/' + latitude + ',' + longitude 
 
     request({ url, json: true }, (error, { body }) => {
@@ -9,9 +9,9 @@ const forcast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Coordinate error', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  The high today is ' + body.daily.data[0].temperatureHigh + 'with a low of ' + body.daily.data[0].temperatureLow + '.  There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
 
-module.exports = forcast
+module.exports = forecast
